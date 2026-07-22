73819dd604485631d14f1c2f88c79d4dc3e48417
= <=>
2026-07-22 18:57:04 -0400

    added request

--- js/json.js
+++ js/json.js
@@ -3,13 +3,15 @@ const header = document.querySelector("header");
 const section = document.querySelector("section");
 
 // STEP 3a: Create the asynchronous function populate()
-
+async function populate(){
 // Introducing JavaScript Object Notation (JSON): https://json.org/
 // STEP 4a: Create i-scream.json file with companyName, headOffice, established, active, topFlavors(name, calories, type, ingredients, image) */
 // STEP 4b: Store the URL of a JSON file in a variable */
+let url = "https://kizzatronreal.github.io/26S-JavaScript-01-Week11/js/icecream.json";
 
 // STEP 5: Use the new URL to create a new request object
-
+let request = new Request(url);
+console.log(request);
 // STEP 6: Make a network request with the fetch() function, which returns a Response object
 
 // STEP 7: Capture the returned Response object and covert to a JSON object using json()
@@ -17,11 +19,11 @@ const section = document.querySelector("section");
 // STEP 8: Output the iScream JSON object to the console
 
 // STEP 9a: Invoke the populateHeader function here, then build it below
-    
+
 // STEP 10a: Invoke the showTopFlavors function here, then build it below
-
+}
 // STEP 3b: Call the populate() function
-
+populate();
 /* STEP 9b: Build out the populateHeader() function */
 
 // Create the H1 element
